92f45c1fc035ce87badcd71e30528d1d1f330fda
iamjpsharma <sjaiprakash457@gmail.com>
2023-02-23 21:09:53 +0530

    ChatGPT

---
+++ README.md
@@ -0,0 +1,7 @@
+# ChatGPT App
+
+## About
+A demo app for chatGPT (OpenAI) with WebView
+
+## Installation
+Download the latest release and install it on your device.
---
+++ app/src/main/java/com/iamjpsharma/chatgpt/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.iamjpsharma.chatgpt
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowManager
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.window.OnBackInvokedDispatcher
+import com.iamjpsharma.chatgpt.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+    private val userAgent =
+        "Mozilla/5.0 (Linux; Android " + Build.VERSION.RELEASE + ") AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.79 Mobile Safari/537.36"
+    private val chatUrl = "https://chat.openai.com/"
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        webView = binding.webView
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            onBackInvokedDispatcher.registerOnBackInvokedCallback(
+                OnBackInvokedDispatcher.PRIORITY_DEFAULT
+            ) {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        }
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.parseColor("#343541")
+
+        webView.settings.userAgentString = userAgent
+        webView.settings.domStorageEnabled = true
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl(chatUrl)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        @Suppress("DEPRECATION")
+        if (webView.canGoBack() && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU)
+            webView.goBack()
+        else
+            super.onBackPressed()
+    }
+}
